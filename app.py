9a56f68edc2fc425bf81ee0dc0bd91c585113571
MelissaLesiewicz <86027932+MelissaLesiewicz@users.noreply.github.com>
2021-08-21 12:29:22 -0400

    new pages

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+import datetime as dt
+from flask.globals import session
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy import engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+from flask import Flask, jsonify
+
+#create engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, relflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
 # create app
 app = Flask(__name__)
 
 # create app route
 @app.route('/')
 
-def hello_world():
-    return 'Hello world'
-
+def welcome():
+    return (
+        '''
+        Welcome to teh Climate Analysis API!
+        Available Routes:
+        /api/v1.0/precipitation
+        /api/v1.0/stations
+        /api/v1.0/tobs
+        /api/v1.0/temp/start/end
+        ''')
+    
