ec9eb7d03839ce088dfc338e18535149a868f716
MelissaLesiewicz <86027932+MelissaLesiewicz@users.noreply.github.com>
2021-08-21 13:08:11 -0400

    commit

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 #create engine
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite://hawaii.sqlite")
 
 Base = automap_base()
-Base.prepare(engine, relflect=True)
+Base.prepare(engine, reflect=True)
 
 Measurement = Base.classes.measurement
 Station = Base.classes.station
@@ -32,7 +32,7 @@ app = Flask(__name__)
 def welcome():
     return (
         '''
-        Welcome to teh Climate Analysis API!
+        Welcome to the Climate Analysis API!
         Available Routes:
         /api/v1.0/precipitation
         /api/v1.0/stations
@@ -40,3 +40,12 @@ def welcome():
         /api/v1.0/temp/start/end
         ''')
     
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    
+    return jsonify(precip)
